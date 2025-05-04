26a3ff6dedc49641a12eaac047fea91fa758ac26
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2025-05-04 14:52:06 +0200

    Create user_model.dart

---
+++ lib/feature_auth/models/user_model.dart
@@ -0,0 +1,39 @@
+class UserModel {
+  final String id;
+  final String name;
+  final String email;
+  final String? profilePicture;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.profilePicture,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+      profilePicture: json['profilePicture'] as String?,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+      updatedAt: DateTime.parse(json['updatedAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'profilePicture': profilePicture,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+    };
+  }
+} 
