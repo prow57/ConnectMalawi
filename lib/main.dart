5130b425e391acce5ca65568c7564a3025c1c938
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2025-05-06 10:13:45 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter/services.dart';
+import 'package:nyasa_send/feature_auth/screens/login_screen.dart';
+import 'package:nyasa_send/feature_auth/screens/register_screen.dart';
 import 'constants/app_constants.dart';
 import 'constants/theme_constants.dart';
 import 'feature_auth/screens/welcome_screen.dart';
 import 'feature_auth/screens/onboarding_screen.dart';
-import 'feature_auth/screens/login_screen.dart';
-import 'feature_auth/screens/register_screen.dart';
-import 'feature_auth/screens/otp_verification_screen.dart';
-import 'feature_auth/screens/forgot_password_screen.dart';
-import 'feature_auth/screens/reset_password_screen.dart';
 import 'feature_dashboard/screens/home_dashboard_screen.dart';
-import 'feature_auth/providers/auth_provider.dart';
-import 'feature_auth/services/auth_service.dart';
+import 'feature_dashboard/screens/send_money_screen.dart';
+import 'feature_dashboard/screens/request_money_screen.dart';
+import 'feature_dashboard/screens/add_bank_screen.dart';
+import 'feature_dashboard/screens/recipients_screen.dart';
+import 'feature_dashboard/screens/accounts_screen.dart';
+import 'feature_dashboard/screens/transactions_screen.dart';
+import 'feature_dashboard/screens/notifications_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
@@ -23,74 +29,85 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<FlutterSecureStorage>(
-          create: (_) => const FlutterSecureStorage(),
+    return MaterialApp(
+      title: AppConstants.appName,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: ThemeConstants.primaryColor,
+        scaffoldBackgroundColor: ThemeConstants.backgroundColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: ThemeConstants.surfaceColor,
+          foregroundColor: ThemeConstants.textPrimaryColor,
+          elevation: 0,
+          centerTitle: true,
         ),
-        Provider<AuthService>(
-          create: (_) => AuthService(
-            baseUrl: AppConstants.apiBaseUrl,
-          ),
-        ),
-        ChangeNotifierProxyProvider2<AuthService, FlutterSecureStorage, AuthProvider>(
-          create: (context) => AuthProvider(
-            authService: context.read<AuthService>(),
-            secureStorage: context.read<FlutterSecureStorage>(),
-          ),
-          update: (context, authService, secureStorage, previous) => AuthProvider(
-            authService: authService,
-            secureStorage: secureStorage,
-          ),
-        ),
-      ],
-      child: MaterialApp(
-        title: AppConstants.appName,
-        theme: ThemeData(
-          primaryColor: ThemeConstants.primaryColor,
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: ThemeConstants.primaryColor,
-            primary: ThemeConstants.primaryColor,
-            secondary: ThemeConstants.secondaryColor,
-          ),
-          scaffoldBackgroundColor: ThemeConstants.backgroundColor,
-          useMaterial3: true,
-          textTheme: const TextTheme(
-            displayLarge: ThemeConstants.heading1,
-            displayMedium: ThemeConstants.heading2,
-            displaySmall: ThemeConstants.heading3,
-            bodyLarge: ThemeConstants.body1,
-            bodyMedium: ThemeConstants.body2,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: ThemeConstants.primaryColor,
+            foregroundColor: Colors.white,
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(ThemeConstants.borderRadiusM),
+            ),
           ),
         ),
-        initialRoute: AppConstants.routeWelcome,
-        routes: {
-          AppConstants.routeWelcome: (context) => const WelcomeScreen(),
-          AppConstants.routeOnboarding: (context) => const OnboardingScreen(),
-          AppConstants.routeLogin: (context) => const LoginScreen(),
-          AppConstants.routeRegister: (context) => const RegisterScreen(),
-          AppConstants.routeVerifyOTP: (context) {
-            final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
-            return OTPVerificationScreen(
-              name: args['name'] ?? '',
-              phone: args['phone'],
-              email: args['email'],
-              password: args['password'],
-              isPasswordReset: args['isPasswordReset'] ?? false,
-            );
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: ThemeConstants.surfaceColor,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(ThemeConstants.borderRadiusM),
+            borderSide: const BorderSide(
+              color: ThemeConstants.textSecondaryColor,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(ThemeConstants.borderRadiusM),
+            borderSide: const BorderSide(
+              color: ThemeConstants.textSecondaryColor,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(ThemeConstants.borderRadiusM),
+            borderSide: const BorderSide(
+              color: ThemeConstants.primaryColor,
+            ),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(ThemeConstants.borderRadiusM),
+            borderSide: const BorderSide(
+              color: ThemeConstants.errorColor,
+            ),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(ThemeConstants.borderRadiusM),
+            borderSide: const BorderSide(
+              color: ThemeConstants.errorColor,
+            ),
+          ),
+        ),
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
           },
-          AppConstants.routeForgotPassword: (context) => const ForgotPasswordScreen(),
-          AppConstants.routeResetPassword: (context) {
-            final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
-            return ResetPasswordScreen(
-              phone: args['phone'],
-              email: args['email'],
-              otp: args['otp'],
-            );
-          },
-          AppConstants.routeHome: (context) => const HomeDashboardScreen(),
-        },
+        ),
       ),
+      initialRoute: AppConstants.routeWelcome,
+      routes: {
+        AppConstants.routeWelcome: (context) => const WelcomeScreen(),
+        AppConstants.routeOnboarding: (context) => const OnboardingScreen(),
+        AppConstants.routeHome: (context) => const HomeDashboardScreen(),
+        AppConstants.routeSendMoney: (context) => const SendMoneyScreen(),
+        AppConstants.routeRequestMoney: (context) => const RequestMoneyScreen(),
+        AppConstants.routeAddBank: (context) => const AddBankScreen(),
+        AppConstants.routeRecipients: (context) => const RecipientsScreen(),
+        AppConstants.routeAccounts: (context) => const AccountsScreen(),
+        AppConstants.routeTransactions: (context) => const TransactionsScreen(),
+        AppConstants.routeNotifications: (context) =>
+            const NotificationsScreen(),
+        AppConstants.routeLogin: (context) => const LoginScreen(),
+        AppConstants.routeRegister: (context) => const RegisterScreen(),
+      },
     );
   }
 }
